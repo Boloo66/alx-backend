41769b77b3ca3c952da1dcc78ed515604af6ea3d
Boloo66 <oguntadeemmanuel66@icloud.com>
2023-01-27 02:45:56 +0100

    cache

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -9,7 +9,7 @@ class BasicCache(BaseCaching):
     """ Inherits from a parent class
     """
 
-    def __init__(self) -> None:
+    def __init__(self):
         """ Constructor method
         """
         super().__init__()
@@ -25,6 +25,6 @@ class BasicCache(BaseCaching):
     def get(self, key):
         """ Returns a key to when called
         """
-        if key in self.cache_data.keys():
+        if key in self.cache_data.keys() and key is not None:
             return self.cache_data.get(key)
         return None
